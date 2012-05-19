c70fe37059ee035699cfcbd813f2a38ee5eacd36
José Luis Herranz <jherranzm@gmail.com>
2012-05-19 17:32:29 +0200

    Definitivo. Cambios estéticos.

--- JsTestDriverTest/src/ControlHORUS.js
+++ JsTestDriverTest/src/ControlHORUS.js
@@ -5,33 +5,31 @@
 
 String.prototype.trim = function() {
 	return this.replace(/^\s+|\s+$/g,"");
-}
+};
 
 String.prototype.ltrim = function() {
 	return this.replace(/^\s+/,"");
-}
+};
 
 String.prototype.rtrim = function() {
 	return this.replace(/\s+$/,"");
-}
+};
 
 myapp = {};
 
 myapp.ControlHORUS = function() { };
 
-myapp.ControlHORUS.prototype.greet = function(name) {
-  return "Hello " + name + "!";
-};
 
 myapp.ControlHORUS.prototype.validaHORUS = function(codigo) {
 	
+	var LONGITUD = 7;
+	var letras  = /[a-zA-Z]/;
 	
 	if(codigo == null || codigo == "") return false;
 	codigo = codigo.trim();
-	if(codigo.length != 7) return false;
+	if(codigo.length != LONGITUD) return false;
 	
 	// El primer caracter ha de ser una letra
-	var letras  = /[a-zA-Z]/;
 	var primeraLetra = codigo.charAt(0);
 	if(!letras.test(primeraLetra)) return false;
 	
@@ -39,6 +37,26 @@ myapp.ControlHORUS.prototype.validaHORUS = function(codigo) {
 		//alert(codigo + " : " + k + ":" + codigo[k] + ":" + isNaN(codigo[k]));
 		if(codigo[k] == " ") return false;
 		if(isNaN(codigo[k])){
+			return false;
+		}
+	}
+	
+	return true;
+};
+
+
+myapp.ControlHORUS.prototype.validaCP = function(codigoPostal) {
+	
+	var LONGITUD = 5;
+	
+	if(codigoPostal == null || codigoPostal == "") return false;
+	codigoPostal = codigoPostal.trim();
+	if(codigoPostal.length != LONGITUD) return false;
+	
+	// Han de ser 5 dígitos
+	for(var k = 0; k < codigoPostal.length; k++){
+		if(codigoPostal[k] == " ") return false;
+		if(isNaN(codigoPostal[k])){
 			return false;
 		}
 	}
