184b6a3283471bafc406abf00cc186d516f58d05
José Luis Herranz <jherranzm@gmail.com>
2012-05-19 11:20:44 +0200

    Queda probar que los últimos 6 caracteres sean dígitos...

--- JsTestDriverTest/src-test/GreeterTest.js
+++ JsTestDriverTest/src-test/GreeterTest.js
@@ -8,3 +8,54 @@ GreeterTest.prototype.testGreet = function() {
   var greeter = new myapp.Greeter();
   assertEquals("Hello World!", greeter.greet("World"));
 };
+
+GreeterTest.prototype.testHORUSNulo = function() {
+	  var greeter = new myapp.Greeter();
+	  assertEquals(false, greeter.validaHORUS(null));
+};
+
+GreeterTest.prototype.testHORUSVacio = function() {
+	  var greeter = new myapp.Greeter();
+	  assertEquals(false, greeter.validaHORUS(""));
+};
+
+GreeterTest.prototype.testHORUSNumerico = function() {
+	  var greeter = new myapp.Greeter();
+	  assertEquals(false, greeter.validaHORUS("1234567"));
+};
+
+GreeterTest.prototype.testHORUSLongitudErronea = function() {
+	  var greeter = new myapp.Greeter();
+	  assertEquals(false, greeter.validaHORUS("123456"));
+};
+
+
+GreeterTest.prototype.testHORUSLongitud5Caracteres = function() {
+	  var greeter = new myapp.Greeter();
+	  assertEquals(false, greeter.validaHORUS("12345"));
+};
+
+GreeterTest.prototype.testHORUSLongitud4Caracteres = function() {
+	  var greeter = new myapp.Greeter();
+	  assertEquals(false, greeter.validaHORUS("1234"));
+};
+
+GreeterTest.prototype.testHORUSCorrectoMin = function() {
+	  var greeter = new myapp.Greeter();
+	  assertEquals(true, greeter.validaHORUS("a234567"));
+};
+
+GreeterTest.prototype.testHORUSCorrectoMay = function() {
+	  var greeter = new myapp.Greeter();
+	  assertEquals(true, greeter.validaHORUS("Z234567"));
+};
+
+GreeterTest.prototype.testHORUSTodoLetras = function() {
+	  var greeter = new myapp.Greeter();
+	  assertEquals(false, greeter.validaHORUS("Zabcdef"));
+};
+
+GreeterTest.prototype.testHORUSCaracter = function() {
+	  var greeter = new myapp.Greeter();
+	  assertEquals(false, greeter.validaHORUS("_234567"));
+};
--- JsTestDriverTest/src/Greeter.js
+++ JsTestDriverTest/src/Greeter.js
@@ -9,3 +9,23 @@ myapp.Greeter = function() { };
 myapp.Greeter.prototype.greet = function(name) {
   return "Hello " + name + "!";
 };
+
+myapp.Greeter.prototype.validaHORUS = function(codigo) {
+	
+	if(codigo == null || codigo == "") return false;
+	if(codigo.length != 7) return false;
+	
+	// El primer caracter ha de ser una letra
+	var letras  = /[a-zA-Z]/;
+	var primeraLetra = codigo.charAt(0);
+	if(!letras.test(primeraLetra)) return false;
+	
+	for(var k = 1; k < codigo.length; k++){
+		//alert(codigo + " : " + k + ":" + codigo[k] + ":" + isNaN(codigo[k]));
+		if(isNaN(codigo[k])){
+			return false;
+		}
+	}
+	
+	return true;
+};
